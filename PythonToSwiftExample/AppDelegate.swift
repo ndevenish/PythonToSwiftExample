9f2a552cd33743f00112032d51cb6fa29c5974da
Nicholas Devenish <ndevenish@gmail.com>
2016-06-17 20:29:57 +0100

    Initial Commit

---
+++ PythonToSwiftExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PythonToSwiftExample
+//
+//  Created by Nicholas Devenish on 17/06/2016.
+//  Copyright © 2016 Nicholas Devenish. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ PythonToSwiftExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PythonToSwiftExample
+//
+//  Created by Nicholas Devenish on 17/06/2016.
+//  Copyright © 2016 Nicholas Devenish. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
